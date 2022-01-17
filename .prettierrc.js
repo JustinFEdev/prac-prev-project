05096a4a339ea50811ea58f9f665aa59cd9d88f2
Justin_supertree <justin1688@supertree.co>
2022-01-17 16:21:38 +0900

    1/17 test

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('./shared/config/.prettierrc.app'),
+};
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header style={{ display: "flex", justifyContent: "center", borderBottom: "1px solid" }}>
+        <h1>새로운 태그를 연습</h1>
       </header>
-    </div>
+      <body style={{ height: "30rem", borderBottom: "1px solid" }}>
+        <div>body area</div>
+      </body>
+      <footer>
+        <div>footer</div>
+      </footer>
+    </>
   );
 }
 
